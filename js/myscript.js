28a076217ced2438b482b35e105529f8f37825fe
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-04 15:44:37 +0200

    fix variables and add ticket id

--- js/myscript.js
+++ js/myscript.js
@@ -3,19 +3,19 @@ let ticketPriceReduced;
 let ticketPriceSenior;
 let priceMessage;
 //Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
-let userKm = parseInt(prompt("Inserisci quanti chilometri vuoi percorrere"));
-let userAge = parseInt(prompt("Inserisci la tua eta"));
+const userKm = parseInt(prompt("Inserisci quanti chilometri vuoi percorrere"));
+const userAge = parseInt(prompt("Inserisci la tua eta"));
 console.log("L'utente percorre",userKm,"km ed ha",userAge,"anni");
 //Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 //il prezzo del biglietto è definito in base ai km (0.21 € al km
 let ticketPrice = (userKm * 0.21).toFixed(2);
 console.log("Il prezzo intero del biglietto è €",ticketPrice);
 
-if(userAge < 18){ //va applicato uno sconto del 20% per i minorenni
+if(userAge < 18) { //va applicato uno sconto del 20% per i minorenni
     ticketPriceReduced = (ticketPrice - ((ticketPrice * 20) / 100)).toFixed(2);
     console.log("Biglietto ridotto €",ticketPriceReduced);
     priceMessage = `Il costo del tuo biglietto è di €${ticketPriceReduced}`;
-} else if(userAge >= 65){ //va applicato uno sconto del 40% per gli over 65.
+} else if(userAge >= 65) { //va applicato uno sconto del 40% per gli over 65.
     ticketPriceSenior = (ticketPrice - ((ticketPrice * 40) / 100)).toFixed(2);
     console.log("Biglietto senior €",ticketPriceSenior);
     priceMessage = `Il costo del tuo biglietto è di €${ticketPriceSenior}`;
@@ -31,3 +31,9 @@ document.getElementById("user-info").innerHTML = `Il biglietto è utilizzabile p
 
 const now = new Date();
 document.getElementById("current-date").innerHTML = now;
+
+//generatore id biglietto
+let ticketId;
+let randomNum =  Math.floor(Math.random() * 10);
+ticketId = `Ticket id ${randomNum}${randomNum + 1}${randomNum + 2}${randomNum + 3}`;
+document.getElementById("ticket-id").innerHTML = ticketId;
