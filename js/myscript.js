44c20b3a4baeb83dba17c384c816cd6fd0e51e0a
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-04 15:30:00 +0200

    add .tofixed to ticket price

--- js/myscript.js
+++ js/myscript.js
@@ -8,15 +8,15 @@ let userAge = parseInt(prompt("Inserisci la tua eta"));
 console.log("L'utente percorre",userKm,"km ed ha",userAge,"anni");
 //Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 //il prezzo del biglietto è definito in base ai km (0.21 € al km
-let ticketPrice = userKm * 0.21;
+let ticketPrice = (userKm * 0.21).toFixed(2);
 console.log("Il prezzo intero del biglietto è €",ticketPrice);
 
 if(userAge < 18){ //va applicato uno sconto del 20% per i minorenni
-    ticketPriceReduced = (ticketPrice - ((ticketPrice * 20) / 100));
+    ticketPriceReduced = (ticketPrice - ((ticketPrice * 20) / 100)).toFixed(2);
     console.log("Biglietto ridotto €",ticketPriceReduced);
     priceMessage = `Il costo del tuo biglietto è di €${ticketPriceReduced}`;
 } else if(userAge >= 65){ //va applicato uno sconto del 40% per gli over 65.
-    ticketPriceSenior = (ticketPrice - ((ticketPrice * 40) / 100));
+    ticketPriceSenior = (ticketPrice - ((ticketPrice * 40) / 100)).toFixed(2);
     console.log("Biglietto senior €",ticketPriceSenior);
     priceMessage = `Il costo del tuo biglietto è di €${ticketPriceSenior}`;
 } else{
